abeee63de8c0f5b46c7c783c29d97f07c3b51582
Никита Колташов <jenkinstix@gmail.com>
2022-01-23 00:51:15 +0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,13 @@ import org.apache.commons.cli.*;
 import services.ChildService;
 import services.EducationService;
 import services.ParentsService;
-
+ 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+ 
 public class Main {
     public static void main(String[] args) {
-        if (new AddressService().getAddress().size() == 0) {
-            System.out.println("mocking data...");
-            MockData();
-        }
-
         Options options = new Options();
         Option addParents = new Option("p", "addParents", false, "add parents");
         options.addOption(addParents);
@@ -33,34 +28,31 @@ public class Main {
         Option addChild = new Option("c", "addChild", true, "add child");
         addChild.setArgs(3);
         options.addOption(addChild);
-
+ 
         CommandLineParser parser = new org.apache.commons.cli.BasicParser();
         HelpFormatter formatter = new HelpFormatter();
         CommandLine cmd = null;
-
+ 
         try {
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
             System.out.println(e.getMessage());
             formatter.printHelp("utility-name", options);
-
+ 
             System.exit(1);
         }
-
+ 
         if (cmd.hasOption('p')) {
             AddParents(cmd);
-        }
-        if (cmd.hasOption('c')) {
+        } else if (cmd.hasOption('c')) {
             AddChild(cmd.getOptionValues('c'));
-        }
-        if (cmd.hasOption('e')) {
+        } else if (cmd.hasOption('e')) {
             ChangeEducational(cmd.getOptionValues('e'));
-        }
-        if (cmd.hasOption('a')) {
+        } else if (cmd.hasOption('a')) {
             ChangeAddress(cmd.getOptionValues('a'));
         }
     }
-
+ 
     private static void ChangeEducational(String[] args) {
         int childId = Integer.parseInt(args[0]);
         int educationalEstablishmentId = Integer.parseInt(args[1]);
@@ -69,7 +61,7 @@ public class Main {
         child.education = new EducationService().getById(educationalEstablishmentId);
         child.update();
     }
-
+ 
     private static void ChangeAddress(String[] args) {
         int parentsId = Integer.parseInt(args[0]);
         int addressId = Integer.parseInt(args[1]);
@@ -78,24 +70,24 @@ public class Main {
         parents.address = new AddressService().getById(addressId);
         parents.update();
     }
-
+ 
     private static void AddChild(String[] args) {
         String fullname = args[0];
         int parentsCoupleId = Integer.parseInt(args[1]);
         int age = Integer.parseInt(args[2]);
         Parents parents = new ParentsService().getById(parentsCoupleId);
-
+ 
         if (parents.address != null) {
             List<Education> educational = new EducationService().getEducation();
             List<Education> fineEducational = educational.stream().filter(x -> parents.address.area.addresses.stream().anyMatch(y -> y.id == x.address.id)).collect(Collectors.toList());
             fineEducational.forEach(x -> System.out.println("Подходящее учебное учреждение! Имя " + x.number + ". Id: " + x.id));
         }
-
+ 
         Child child = new Child(fullname, parents, age);
         child.save();
         System.out.println("Ребенок добавлен. Id " + child.id);
     }
-
+ 
     private static void AddParents(CommandLine args) {
         Parents parents = new Parents();
         if (args.hasOption('m')) {
@@ -108,57 +100,5 @@ public class Main {
         }
         parents.save();
         System.out.println("Родитель(и) добавлен(ы). Id " + parents.id);
-    }
-
-    // Делаем 3 района, в каждом районе по 2 учебных учреждения.
-    public static void MockData() {
-        Area[] areas = new Area[]{
-                new Area(),
-                new Area(),
-                new Area(),
-        };
-        for (Area area : areas) {
-            area.Save();
-        }
-        int i = 0;
-        List<Address> addresses = new ArrayList<>(25);
-        for (String addressName : new String[]{
-                "76209 Blue Bill Park Center",
-                "113 Lakewood Road",
-                "164 Bunker Hill Hill",
-                "36 Rutledge Circle",
-                "15895 Summit Center",
-                "414 Warrior Park",
-                "155 Bartelt Crossing",
-                "04 Sheridan Street",
-                "43701 2nd Terrace",
-                "3999 Fremont Parkway",
-                "59571 Fordem Drive",
-                "3576 Spohn Road",
-                "16 Lotheville Plaza",
-                "2642 Delaware Street",
-                "28 Sage Plaza",
-                "94 Moulton Parkway",
-                "38 Johnson Drive",
-                "548 Scott Drive",
-                "4829 Bay Center",
-                "83 Pepper Wood Junction",
-                "8 Anthes Crossing",
-                "826 Fairfield Park",
-                "44 Dottie Terrace",
-                "09502 Oakridge Court",
-                "55 Armistice Center",
-        }) {
-            Address address = new Address(addressName);
-            addresses.add(address);
-            address.area = areas[i % 3];
-            address.Save();
-            i++;
-        }
-
-        for (i = 0; i < 6; i++) {
-            Education education = new Education("School #" + i, addresses.get(i));
-            education.Save();
-        }
     }
 }
